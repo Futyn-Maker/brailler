ee08920efa462c588f8eb078eecd5fe830274a26
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-07-15 13:28:05 +0200

    Fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://infolektuell.github.io/brailler/',
+  site: 'https://infolektuell.github.io',
+  base: '/brailler',
+  trailingSlash: 'always',
   integrations: [svelte()],
 })
