5762190125976064ae6334a507e985af0c667ba3
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-07-15 11:01:50 +0200

    Add brailler code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config'
-
 import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://infolektuell.github.io/brailler/',
   integrations: [svelte()],
 })
---
+++ src/components/Brailler.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+import { type BrailleKeys, braille, isNoChar, Brailler } from './braille'
+let keys: BrailleKeys = [' ', '4', '3', '2', '5', '6', '7', '1', '8']
+const dotFields = [7, 3, 2, 1, 0, 4, 5, 6, 8]
+const handleKeyup = (e: KeyboardEvent, index: number) => {
+  if (isNoChar(e)) {
+    return
+  }
+  e.preventDefault()
+  keys[index] = e.key
+  keys = keys
+}
+let value: string
+</script>
+
+<section class="box" aria-label="Keyboard settings">
+  <div class="columns">
+    {#each dotFields as dot}
+      <div class="column">
+        <div class="field">
+          <label class="label" for="{'dot' + dot + 'input'}"
+            ><span class="braille">{String.fromCharCode(10240 + Brailler.BrailleDots[dot])}</span></label
+          >
+          <div class="control">
+            <input
+              class="input is-small"
+              type="text"
+              size="1"
+              minlength="1"
+              maxlength="1"
+              name="{'dot' + dot}"
+              id="{'dot' + dot + 'input'}"
+              on:keyup="{(e) => {
+                handleKeyup(e, dot)
+              }}"
+              value="{keys[dot]}"
+            />
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
+</section>
+<section class="box" aria-label="Text input">
+  <div class="field">
+    <label for="braille-input">Braille Input</label>
+    <div class="control">
+      <textarea id="braille-input" class="textarea" use:braille="{{ keys }}" bind:value></textarea>
+    </div>
+  </div>
+</section>
+
+<style>
+.columns {
+  width: fit-content;
+}
+.braille {
+  border-style: dashed;
+  border-color: lightgray;
+  border-width: thin;
+  padding: 0;
+}
+</style>
